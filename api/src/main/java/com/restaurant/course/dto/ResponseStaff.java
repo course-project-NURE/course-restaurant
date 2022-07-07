629f2802eb4fb65eb562d6d678278acdfef580de
Artem Pukha <artem.pukha@vizientinc.com>
2022-07-07 14:36:32 +0300

    Refactor dtos

--- api/src/main/java/com/restaurant/course/dto/ResponseStaff.java
+++ api/src/main/java/com/restaurant/course/dto/ResponseStaff.java
@@ -1,15 +1,18 @@
 package com.restaurant.course.dto;
 
 import com.restaurant.course.entity.Role;
+import com.restaurant.course.entity.Staff;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Objects;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ResponseStaff {
-    private String id;
+    private Integer id;
     private String email;
     private String name;
     private String surname;
@@ -18,16 +21,40 @@ public class ResponseStaff {
     private Integer salary;
     private Role role;
 
+    public ResponseStaff(Staff staff){
+        this.setFromStaff(staff);
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ResponseStaff that = (ResponseStaff) o;
-        return getEmail().equals(that.getEmail()) && getName().equals(that.getName()) && getSurname().equals(that.getSurname()) && getLastname().equals(that.getLastname()) && getPhone().equals(that.getPhone()) && getSalary().equals(that.getSalary()) && getRole() == that.getRole();
+        return getId().equals(that.getId()) &&
+                getEmail().equals(that.getEmail()) &&
+                getName().equals(that.getName()) &&
+                getSurname().equals(that.getSurname()) &&
+                getLastname().equals(that.getLastname()) &&
+                getPhone().equals(that.getPhone()) &&
+                getSalary().equals(that.getSalary()) &&
+                getRole() == that.getRole();
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getEmail(), getName(), getSurname(), getLastname(), getPhone(), getSalary(), getRole());
+        return Objects.hash(getId(),
+                getEmail(), getName(), getSurname(), getLastname(), getPhone(), getSalary(), getRole());
+    }
+
+
+    public void setFromStaff(Staff staff){
+        this.id = staff.getId();
+        this.email = staff.getLoginInfo().getEmail();
+        this.name = staff.getName();
+        this.surname = staff.getSurname();
+        this.lastname = staff.getLastname();
+        this.phone = staff.getPhone();
+        this.salary = staff.getSalary();
+        this.role = staff.getLoginInfo().getRole().getTitle();
     }
 }
